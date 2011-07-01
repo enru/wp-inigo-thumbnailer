dcc12fc36d889022c6772ceb50c547dabb75010a
Neill Russell <neill@enru.co.uk>
2011-07-01 18:19:35 +0100

    soft tabs

--- inigo/thumbnailcacher.php
+++ inigo/thumbnailcacher.php
@@ -1,12 +1,12 @@
 <?php
 /*
-	Plugin Name: Thumbnail Create & Cache Plugin
-	Plugin URI: http://inigo.net
-	Description: creates thumbnails & caches them 
-	Version: 0.1
-	Author: Neill Russell
-	Author URI: http://enru.co.uk
-	License: GPL2 
+    Plugin Name: Thumbnail Create & Cache Plugin
+    Plugin URI: http://inigo.net
+    Description: creates thumbnails & caches them 
+    Version: 0.1
+    Author: Neill Russell
+    Author URI: http://enru.co.uk
+    License: GPL2 
 */
 /*  Copyright 2011 Neill Russell & Inigo Media Ltd (email: neill@inigo.net)
 
@@ -26,89 +26,89 @@
 
 class Inigo_Thumbnailer {
     var $cache = null;
-	function __construct($dir) {
-		$this->cache = get_template_directory() . '/'. $dir;
-		if(!file_exists($this->cache)) {
-			mkdir($this->cache, 0755, true);
-		}
-	}
-	function cached($original) {
-		$original_path = $this->path($original);
-		if(!file_exists($original_path)) return $original; 
-		$thumb = $this->thumb($original_path);
-		if(!file_exists($thumb)) {
-			$this->create($thumb, $original_path, 168, 168);
-		}
-		$parts = explode('/wp-content', $thumb);
-		$uri = content_url() . $parts[1];
-		return $uri;
-	}
-	function path($url) {
-		$root = preg_replace('/themes$/', '', get_theme_root());
-		$parts = explode('/wp-content', $url);
-		return $root . $parts[1];
-	}
-	function thumb($original) {
-		return $this->cache . '/' . basename($original);
-	}
- 	function create($thumb, $original, $newWidth, $newHeight) {
+    function __construct($dir) {
+        $this->cache = get_template_directory() . '/'. $dir;
+        if(!file_exists($this->cache)) {
+            mkdir($this->cache, 0755, true);
+        }
+    }
+    function cached($original) {
+        $original_path = $this->path($original);
+        if(!file_exists($original_path)) return $original; 
+        $thumb = $this->thumb($original_path);
+        if(!file_exists($thumb)) {
+            $this->create($thumb, $original_path, 168, 168);
+        }
+        $parts = explode('/wp-content', $thumb);
+        $uri = content_url() . $parts[1];
+        return $uri;
+    }
+    function path($url) {
+        $root = preg_replace('/themes$/', '', get_theme_root());
+        $parts = explode('/wp-content', $url);
+        return $root . $parts[1];
+    }
+    function thumb($original) {
+        return $this->cache . '/' . basename($original);
+    }
+    function create($thumb, $original, $newWidth, $newHeight) {
         
         // calculate new proportional sizes
 
-		list($width, $height, $image_type) = getimagesize($original);
+        list($width, $height, $image_type) = getimagesize($original);
 
-		$proportionalWidth = $newWidth;
-		$proportionalHeight = $newHeight;
-		if($width / $height >= $newWidth / $newHeight) {
-				$proportionalHeight = round(($newWidth / $width) * $height);
-		}
-		else {
-				$proportionalWidth = round(($newHeight / $height) * $width);
-		}
+        $proportionalWidth = $newWidth;
+        $proportionalHeight = $newHeight;
+        if($width / $height >= $newWidth / $newHeight) {
+            $proportionalHeight = round(($newWidth / $width) * $height);
+        }
+        else {
+           $proportionalWidth = round(($newHeight / $height) * $width);
+        }
 
-		$x = round(($newWidth - $proportionalWidth)/2);
-		$y = round(($newHeight - $proportionalHeight)/2);
+        $x = round(($newWidth - $proportionalWidth)/2);
+        $y = round(($newHeight - $proportionalHeight)/2);
         
-		// create new resized image
+        // create new resized image
         $resized = imagecreatetruecolor($newWidth, $newHeight); // better quality then plain imagecreate
 
-		// fill background
-		$white = imagecolorallocate($resized, 255, 255, 255);
-		imagefill($resized, 0, 0, $white);
+        // fill background
+        $white = imagecolorallocate($resized, 255, 255, 255);
+        imagefill($resized, 0, 0, $white);
 
-		// create source image
-		switch ($image_type) {
-        	case IMAGETYPE_GIF: 
-				$source = imagecreatefromgif($original); 
-				break;
-        	case IMAGETYPE_JPEG: 
-				$source = imagecreatefromjpeg($original);  
-				break;
-        	case IMAGETYPE_PNG: 
-				$source = imagecreatefrompng($original); 
-				break;
-        	default: return; break;
-		}
+        // create source image
+        switch ($image_type) {
+            case IMAGETYPE_GIF: 
+                $source = imagecreatefromgif($original); 
+                break;
+            case IMAGETYPE_JPEG: 
+                $source = imagecreatefromjpeg($original);  
+                break;
+            case IMAGETYPE_PNG: 
+                $source = imagecreatefrompng($original); 
+                break;
+            default: return; break;
+        }
         
-		// resize
-		if($source) {
-				$success = imagecopyresampled($resized, $source, $x, $y, 0, 0,$proportionalWidth, $proportionalHeight, $width, $height);
-		}
+        // resize
+        if($source) {
+           $success = imagecopyresampled($resized, $source, $x, $y, 0, 0,$proportionalWidth, $proportionalHeight, $width, $height);
+        }
         
-		if(is_file($thumb)) { unlink($thumb); }
+        if(is_file($thumb)) { unlink($thumb); }
 
-		switch ($image_type) {
-        	case IMAGETYPE_GIF: imagegif($resized, $thumb); break;
-        	case IMAGETYPE_JPEG: imagejpeg($resized, $thumb, 100);  break; // best quality
-        	case IMAGETYPE_PNG: imagepng($resized, $thumb, 0); break; // no compression
-        	default: break;
-		}
-	}
+        switch ($image_type) {
+            case IMAGETYPE_GIF: imagegif($resized, $thumb); break;
+            case IMAGETYPE_JPEG: imagejpeg($resized, $thumb, 100);  break; // best quality
+            case IMAGETYPE_PNG: imagepng($resized, $thumb, 0); break; // no compression
+            default: break;
+        }
+    }
 
 }
 
 function inigo_thumb($img_path) {
-	$t = new Inigo_Thumbnailer($cache='images/thumbnails');
-	return $t->cached($img_path);
+    $t = new Inigo_Thumbnailer($cache='images/thumbnails');
+    return $t->cached($img_path);
 }
 
